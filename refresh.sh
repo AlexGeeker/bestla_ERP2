434958bc3eae54b4cab8b3d1a0420912e3502d2a
AlexGeeker <277823127@qq.com>
2017-03-06 11:54:26 +0800

    delete express update line

--- refresh.sh
+++ refresh.sh
@@ -1,2 +1,1 @@
-echo 13 | python /python_Project/bestla_ERP/MyPackage/main.py >/python_Project/bestla_ERP/log/shell_logfile_python.log 2>&1
 /usr/bin/supervisorctl reload >/python_Project/bestla_ERP/log/shell_logfile_reload.log 2>&1
