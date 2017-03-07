8bfc1539af9b8d7541eb014e08130c18c45357dd
AlexGeeker <277823127@qq.com>
2017-03-07 09:18:54 +0800

    put two forms at one page

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@
 
 import shutil,os,csv,urllib2,json,time,datetime
 from datetime import date,timedelta
-from flask import render_template,redirect,url_for,send_from_directory,request
+from flask import render_template,redirect,url_for,send_from_directory,request,flash
 from werkzeug.utils import secure_filename
 from ..models import Record,Last_Month_Record
 from . import main
@@ -20,16 +20,15 @@ first = today.replace(day=1)
 lastMon = first - datetime.timedelta(days=1)
 lastMonth = lastMon.strftime("%Y%m")
 
-
 @main.route('/', methods=['GET', 'POST'])
 def index():
     # 从网页获取单号
     findDanhao = []
-    form = NameForm()
-    if form.validate_on_submit():
-        danhao = form.name.data
+    form1 = NameForm()
+    if form1.submit1.data and form1.validate_on_submit():
+        danhao = form1.name.data
         danhao = danhao.strip()  # strip()的作用是去掉左右空格
-        form.name.data = ''
+        form1.name.data = ''
         # 查询单号（本月及上月）
         findDanhao = dbs.session.query(Record).filter(Record.DANHAO == danhao).all() + dbs.session.query(Last_Month_Record).filter(Last_Month_Record.DANHAO == danhao).all()
 
@@ -75,21 +74,18 @@ def index():
     data["last"] = lastMonth_kuaidicount
     data["last_everyone"] = lastMonth_everyoneCount
 
-    return render_template('index.html', form=form, findDanhao=findDanhao, total=allCount, data = data)
-
-@main.route('/api/upload', methods=['GET', 'POST'], strict_slashes=False)
-def api_upload():
-    form = UploadForm()
+    ### 上传文件部分 ###
+    form2 = UploadForm()
     fname = None
     name = None
     selected_date = None
     existed_record = 0
     recorded = 0
-    if form.validate_on_submit():
+    if form2.submit2.data and form2.validate_on_submit():
 
-        selected_date = form.date.data
-        name = form.name.data
-        fname = secure_filename(form.file.data.filename)
+        selected_date = form2.date.data
+        name = form2.name.data
+        fname = secure_filename(form2.file.data.filename)
 
         # 获取文件名
         file_dir = os.path.join(basedir, UPLOAD_FOLDER)  # 文件路径
@@ -104,9 +100,72 @@ def api_upload():
                 if filename == fname:
                     ii = ii + 1
         if ii == 0:
-            form.file.data.save(os.path.join(file_dir, fname))  # 保存文件到upload目录
+            form2.file.data.save(os.path.join(file_dir, fname))  # 保存文件到upload目录
         else:
-            return render_template('upload.html', form=form, filename=fname, message=u'该文件已存在，请重新上传！', name=name,
+            return render_template('index.html', form1=form1, form2=form2, filename=fname,data = data, message=u'该文件已存在，请重新上传！', name=name,
+                                   date=selected_date, existed_record=existed_record, recorded=recorded)
+
+        # 读取文件，并对比数据库
+        with open(os.path.join(file_dir, fname)) as csvFile:
+            new_record = None
+            dictreader = csv.DictReader(csvFile)
+            for line in dictreader:
+                danhao = line['\xcc\xf5\xc2\xeb'].strip()
+                i = dbs.session.query(Record).filter(Record.DANHAO == danhao).count() + dbs.session.query(
+                    Last_Month_Record).filter(Last_Month_Record.DANHAO == danhao).count()
+                if i > 0:  # 如果单号已经存在于数据中
+                    existed_record = existed_record + 1
+                elif i == 0:  # 若单号不在数据库中，则写入数据库
+                    recorded = recorded + 1
+                    if selected_date[0:4] + selected_date[5:7] == lastMonth:
+                        new_record = Last_Month_Record(name, danhao, "0", selected_date)
+                    elif selected_date[0:4] + selected_date[5:7] == currentMonth:
+                        new_record = Record(name, danhao, "0", selected_date)
+                    dbs.session.add(new_record)
+                elif recorded > 50:
+                    dbs.session.commit()
+        dbs.session.commit()
+
+        # 创建当天日期文件夹，并将文件转移至该文件夹
+        currentday_file_dir = os.path.join(file_dir, selected_date)
+        if not os.path.exists(currentday_file_dir):  # 如果文件夹不存在，则创建文件夹
+            os.makedirs(currentday_file_dir)
+        shutil.move(os.path.join(file_dir, fname), currentday_file_dir)
+
+    return render_template('index.html', form1=form1, findDanhao=findDanhao, total=allCount, data = data,form2=form2, filename=fname, name=name, date=selected_date,
+                           existed_record=existed_record, recorded=recorded)
+
+"""
+@main.route('/api/upload', methods=['GET', 'POST'], strict_slashes=False)
+def api_upload():
+    form2 = UploadForm()
+    fname = None
+    name = None
+    selected_date = None
+    existed_record = 0
+    recorded = 0
+    if form2.submit2.data and form2.validate_on_submit():
+
+        selected_date = form2.date.data
+        name = form2.name.data
+        fname = secure_filename(form2.file.data.filename)
+
+        # 获取文件名
+        file_dir = os.path.join(basedir, UPLOAD_FOLDER)  # 文件路径
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)  # 如果文件夹不存在，则创建文件夹
+
+        # 文件名重复判断
+        ii = 0
+        print file_dir
+        for parent, dirnames, filenames in os.walk(file_dir):  # 遍历目录并调用数据库写入函数
+            for filename in filenames:
+                if filename == fname:
+                    ii = ii + 1
+        if ii == 0:
+            form2.file.data.save(os.path.join(file_dir, fname))  # 保存文件到upload目录
+        else:
+            return render_template('upload.html', form1=form2, filename=fname, message=u'该文件已存在，请重新上传！', name=name,
                                    date=selected_date, existed_record=existed_record, recorded=recorded)
 
         # 读取文件，并对比数据库
@@ -135,9 +194,9 @@ def api_upload():
             os.makedirs(currentday_file_dir)
         shutil.move(os.path.join(file_dir, fname), currentday_file_dir)
 
-    return render_template('upload.html', form=form, filename=fname, name=name, date=selected_date,
+    return render_template('upload.html', form2=form2, filename=fname, name=name, date=selected_date,
                            existed_record=existed_record, recorded=recorded)
-
+"""
 
 # 显示上个月每天每人发件数
 @main.route('/<user_name>', methods=['GET', 'POST'])
